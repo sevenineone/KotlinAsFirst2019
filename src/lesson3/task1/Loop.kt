a7e92d122b4cb186f1f20400c60a7f43b96e4b97
sevenineone <alexeyka791@gmail.com>
2019-09-29 13:40:32 +0200

    less0n 3 done

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -145,7 +145,7 @@ fun isCoPrime(m: Int, n: Int): Boolean = gcd(m, n) == 1
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean{
+fun squareBetweenExists(m: Int, n: Int): Boolean{  // её циклами надо было или как?
     var kn = sqrt(n.toDouble()).toInt()
     var km = sqrt(m.toDouble()).toInt()
     return kn > km || sqr(kn) == n || sqr(km) == m
